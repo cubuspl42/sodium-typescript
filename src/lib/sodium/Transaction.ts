18843cd70789a6e748a656ca7e048d253c274db8
cubuspl42 <cubuspl42@gmail.com>
2020-08-21 16:09:01 +0200

    Reformat `Transaction.ts` file

--- src/lib/sodium/Transaction.ts
+++ src/lib/sodium/Transaction.ts
@@ -1,158 +1,158 @@
-import {Vertex, ListenerVertex} from './Vertex';
+import { Vertex } from './Vertex';
 
 let enableDebugFlag = false;
 
 function log(a: any): void {
-  if (enableDebugFlag) {
-    console.log(a);
-  }
+    if (enableDebugFlag) {
+        console.log(a);
+    }
 }
 
 class Set_<A> {
-  private readonly array: Array<A | undefined> = [];
+    private readonly array: Array<A | undefined> = [];
 
-  private _size = 0;
+    private _size = 0;
 
-  get size(): number {
-    return this._size;
-  }
-
-  add(a: A): void {
-    if (this._size < this.array.length) {
-      this.array[this._size] = a;
-      this._size++;
-    } else {
-      this.array.push(a);
-      this._size = this.array.length;
-    }
-  }
-
-  forEach(f: (a: A) => void) {
-    const array = this.array;
-    const size = this.size;
-
-    for (let i = 0; i < size; ++i) {
-      f(array[i]!);
-    }
-  }
-
-  clear(): void {
-    const array = this.array;
-    const size = this.size;
-
-    for (let i = 0; i < size; ++i) {
-      array[i] = undefined;
+    get size(): number {
+        return this._size;
     }
 
-    this._size = 0;
-  }
+    add(a: A): void {
+        if (this._size < this.array.length) {
+            this.array[this._size] = a;
+            this._size++;
+        } else {
+            this.array.push(a);
+            this._size = this.array.length;
+        }
+    }
+
+    forEach(f: (a: A) => void) {
+        const array = this.array;
+        const size = this.size;
+
+        for (let i = 0; i < size; ++i) {
+            f(array[i]!);
+        }
+    }
+
+    clear(): void {
+        const array = this.array;
+        const size = this.size;
+
+        for (let i = 0; i < size; ++i) {
+            array[i] = undefined;
+        }
+
+        this._size = 0;
+    }
 }
 
 function _visit(set: Set_<Vertex>, vertex: Vertex): void {
-  if (vertex.visitedRaw) return;
+    if (vertex.visitedRaw) return;
 
-  vertex.markVisited();
+    vertex.markVisited();
 
-  vertex.dependents?.forEach((v) => {
-    _visit(set, v);
-  });
+    vertex.dependents?.forEach((v) => {
+        _visit(set, v);
+    });
 
-  set.add(vertex);
+    set.add(vertex);
 }
 
 let nextId = 0;
 
 export class Transaction {
-  readonly id = ++nextId;
+    readonly id = ++nextId;
 
-  private roots = new Set<Vertex>();
+    private roots = new Set<Vertex>();
 
-  private static visited = new Set_<Vertex>();
+    private static visited = new Set_<Vertex>();
 
-  private postQueue: Array<() => void> = [];
+    private postQueue: Array<() => void> = [];
 
-  constructor() {
-  }
-
-  addRoot(root: Vertex) {
-    this.roots.add(root);
-  }
-
-  visit(v: Vertex): void {
-      _visit(Transaction.visited, v);
-  }
-
-  close(): void {
-      // TODO: Handle in-transaction errors!
-    const dfs = (roots: Set<Vertex>): Set_<Vertex> => {
-      roots.forEach((v) => {
-          this.visit(v);
-      });
-      return Transaction.visited;
+    constructor() {
     }
 
-    const visited = dfs(this.roots);
-
-    Transaction.visitedVerticesCount = Math.max(visited.size, Transaction.visitedVerticesCount);
-
-    this.roots.clear();
-
-    visited.forEach((v) => {
-      v.process();
-    });
-
-    visited.forEach((v) => {
-      v.postprocess();
-    });
-
-    this.postQueue.forEach((h) => h());
-
-    visited.forEach((l) => l.update());
-
-    Transaction.visited.clear();
-  }
-
-  static visitedVerticesCount = 0;
-
-  static currentTransaction?: Transaction;
-
-  static enableDebug(flag: boolean) {
-    enableDebugFlag = flag;
-  }
-
-  static log(msg: () => any): void {
-    if (enableDebugFlag) {
-      console.log(msg());
+    addRoot(root: Vertex) {
+        this.roots.add(root);
     }
-  }
 
-  public static run<A>(f: (t: Transaction) => A): A {
-    const ct = this.currentTransaction;
-
-    if (!ct) {
-      log(`Transaction start`);
-
-      const t = new Transaction();
-
-      this.currentTransaction = t;
-
-      try {
-        const a = f(t);
-
-        t.close();
-
-        return a;
-      } finally {
-        this.currentTransaction = undefined;
-
-        log(`Transaction end`);
-      }
-    } else {
-      return f(ct);
+    visit(v: Vertex): void {
+        _visit(Transaction.visited, v);
     }
-  }
 
-  public static post<A>(h: () => void): void {
-    Transaction.currentTransaction!.postQueue.push(h);
-  }
+    close(): void {
+        // TODO: Handle in-transaction errors!
+        const dfs = (roots: Set<Vertex>): Set_<Vertex> => {
+            roots.forEach((v) => {
+                this.visit(v);
+            });
+            return Transaction.visited;
+        }
+
+        const visited = dfs(this.roots);
+
+        Transaction.visitedVerticesCount = Math.max(visited.size, Transaction.visitedVerticesCount);
+
+        this.roots.clear();
+
+        visited.forEach((v) => {
+            v.process();
+        });
+
+        visited.forEach((v) => {
+            v.postprocess();
+        });
+
+        this.postQueue.forEach((h) => h());
+
+        visited.forEach((l) => l.update());
+
+        Transaction.visited.clear();
+    }
+
+    static visitedVerticesCount = 0;
+
+    static currentTransaction?: Transaction;
+
+    static enableDebug(flag: boolean) {
+        enableDebugFlag = flag;
+    }
+
+    static log(msg: () => any): void {
+        if (enableDebugFlag) {
+            console.log(msg());
+        }
+    }
+
+    public static run<A>(f: (t: Transaction) => A): A {
+        const ct = this.currentTransaction;
+
+        if (!ct) {
+            log(`Transaction start`);
+
+            const t = new Transaction();
+
+            this.currentTransaction = t;
+
+            try {
+                const a = f(t);
+
+                t.close();
+
+                return a;
+            } finally {
+                this.currentTransaction = undefined;
+
+                log(`Transaction end`);
+            }
+        } else {
+            return f(ct);
+        }
+    }
+
+    public static post<A>(h: () => void): void {
+        Transaction.currentTransaction!.postQueue.push(h);
+    }
 }
